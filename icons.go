0442122c48f38416b5343f94868db3289c0e5c97
kitagawa <kitadrum50@gmail.com>
2019-10-05 14:26:49 +0900

    Delete unnecessary code

--- icons.go
+++ icons.go
@@ -2,24 +2,6 @@ package main
 
 import "github.com/fatih/color"
 
-var (
-	brown       = "905532"
-	aqua        = "3AFFDB"
-	blue        = "689FB6"
-	darkBlue    = "44788E"
-	purple      = "834F79"
-	lightPurple = "834F79"
-	red         = "AE403F"
-	beige       = "F5C06F"
-	yellow      = "F09F17"
-	orange      = "D4843E"
-	darkOrange  = "F16529"
-	pink        = "CB6F6F"
-	green       = "8FAA54"
-	lightGreen  = "31B53E"
-	white       = "FFFFFF"
-)
-
 type Icon struct {
 	Icon  string
 	Color color.Attribute
